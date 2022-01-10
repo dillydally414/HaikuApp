5a9850d1fb6f78f51331772b3255f61e826d74f6
Dillon Scott <dillonscott.2024@gmail.com>
2022-01-10 03:21:27 -0500

    gear icon for settings

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import styled from 'styled-components/native';
-import { Pressable, Text, View } from 'react-native';
+import { Image, Pressable, Text, View } from 'react-native';
 import Haiku from '../components/Haiku';
 import { ReactElement, useState } from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../types';
+import Gear from '../../assets/gear.png';
 
 const Container = styled(View)`
   align-items: center;
@@ -15,9 +16,20 @@ const Container = styled(View)`
   width: 100%;
 `;
 
+const GearImage = styled(Image)`
+  height: 30px;
+  margin: 6% 3% 0 0;
+  opacity: 0.4;
+  width: 30px;
+`;
+
+const GearPressable = styled(Pressable)`
+  align-self: flex-end;
+`;
+
 const Title = styled(Text)`
   font-size: 30px;
-  padding: 35% 0 25%;
+  padding: 25% 0 20%;
 `;
 
 const Home = ({
@@ -28,14 +40,14 @@ const Home = ({
   return (
     <Pressable onPress={() => setBlur(true)}>
       <Container>
+        <GearPressable onPress={() => navigation.navigate('Settings')}>
+          <GearImage source={Gear} />
+        </GearPressable>
         <Title>
           Haiku Buddy
         </Title>
         <StatusBar style="auto" />
         <Haiku blurHandler={[blur, setBlur]} />
-        <Title onPress={() => navigation.navigate('Settings')}>
-          Settings
-        </Title>
       </Container>
     </Pressable>
   );

45af465bf57845387f719a39e15a518d21723dd2
Dillon Scott <dillonscott.2024@gmail.com>
2022-01-10 04:31:07 -0500

    password protection

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,8 +1,9 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React, { ReactElement, useState } from "react";
-import { Alert, Text, TextInput } from "react-native";
+import React, { ReactElement, useEffect, useState } from "react";
+import { Alert, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { RootStackParamList } from "../types";
 import styled from "styled-components/native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Password = styled(TextInput)`
   align-self: center;
@@ -12,6 +13,37 @@ const Password = styled(TextInput)`
   width: 40%;
 `;
 
+const SettingsLabel = styled(Text)`
+  align-self: center;
+  margin-top: 10%;
+`;
+
+const PasswordRow = styled(View)`
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const NewPassword = styled(Password)`
+  align-self: center;
+  border-bottom-width: 1px;
+  border-bottom-color: gray;
+  margin: 5% 0;
+  width: 40%;
+`;
+
+const SubmitNewPassword = styled(TouchableOpacity)`
+  align-self: center;
+  background-color: #0088FF;
+  border-radius: 10px;
+  margin-left: 10%;
+  padding: 1.5% 3%;
+`;
+
+const SubmitText = styled(Text)`
+  align-self: center;
+  color: white;
+`;
+
 const Settings = ({
   navigation,
 }: {
@@ -19,7 +51,8 @@ const Settings = ({
 }): ReactElement => {
   const [signedIn, setSignedIn] = useState(false);
   const [password, setPassword] = useState('');
-  const secretPassword = 'pass';
+  const [newPassword, setNewPassword] = useState('');
+  const [secretPassword, setSecretPassword] = useState('');
 
   const handleSignInAttempt = () => {
     if (password === secretPassword) {
@@ -30,6 +63,20 @@ const Settings = ({
     }
   }
 
+  const changePassword = (password: string) => {
+    AsyncStorage.setItem('password', password);
+    Alert.alert('Password changed successfully', `New password: '${password}'`);
+    setNewPassword('');
+  }
+
+  useEffect(() => {
+    const getOldPass = async () => {
+      const pass = await AsyncStorage.getItem('password');
+      pass ? setSecretPassword(pass) : handleSignInAttempt();
+    }
+    getOldPass();
+  }, []);
+
   return !signedIn ? (
     <Password
       placeholder="Enter password here..."
@@ -39,9 +86,26 @@ const Settings = ({
       returnKeyType="go"
     />
   ) : (
-    <Text>
-      You got access to the settings page! Congrats!
-    </Text>
+    <ScrollView>
+      <SettingsLabel>
+        Set password:
+      </SettingsLabel>
+      <PasswordRow>
+        <NewPassword
+          placeholder="Enter new password here..."
+          onChangeText={(text) => setNewPassword(text)}
+          value={newPassword}
+          returnKeyType="done"
+        />
+        <SubmitNewPassword
+          onPress={() => changePassword(newPassword)}
+        >
+          <SubmitText>
+            Submit
+          </SubmitText>
+        </SubmitNewPassword>
+      </PasswordRow>
+    </ScrollView>
   );
 };
 

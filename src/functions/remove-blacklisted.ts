b502c44319a7ca8fac017e4f9078b1f7f96367de
Dillon Scott <dillonscott.2024@gmail.com>
2022-01-10 05:45:11 -0500

    change filtering function

--- src/functions/remove-blacklisted.ts
+++ src/functions/remove-blacklisted.ts
@@ -1,14 +1,12 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const removeBlacklisted = (text: string, setText: (arg: string) => void): void => {
-  let newText = text;
+  let newText: Array<string> = text.split(' ');
 
   AsyncStorage.getItem('blacklist').then((value) => {
     const blacklistedArray: Array<string> = value ? JSON.parse(value) : [];
-    blacklistedArray.forEach((badWord) => {
-      newText = newText.replace(badWord, '');
-    });
-    setText(newText);
+    newText = newText.filter((word: string) => !blacklistedArray.includes(word.toLowerCase()));
+    setText(newText.join(' '));
   });
 };
 

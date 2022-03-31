d56d81e801d647129d203ef4662c5a8f31c7ca83
Dillon Scott <dillonscott.2024@gmail.com>
2022-03-31 13:21:39 -0400

    Add cliboard support

--- src/pages/Finished.tsx
+++ src/pages/Finished.tsx
@@ -2,12 +2,22 @@ import { RootStackParamList } from "../types";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { ReactElement } from "react";
 import { Button, ScrollView, Text, View } from "react-native";
+import * as Clipboard from 'expo-clipboard';
+import { useState, useEffect } from "react";
 
 const Finished = ({
   navigation,
   route: { params: { haiku } }
 }: NativeStackScreenProps<RootStackParamList, 'Finished'>
 ): ReactElement => {
+  const [copied, setCopied] = useState(true);
+
+  useEffect(() => {
+    if (copied) {
+      setTimeout(() => setCopied(false), 3000)
+    }
+  }, [copied])
+
   return (
     <ScrollView>
       <View style={{ marginTop: 100 }}>
@@ -18,6 +28,18 @@ const Finished = ({
             </Text>
           );
         })}
+        <Button
+          title='Copy to Clipboard'
+          onPress={() => {
+            Clipboard.setString(haiku.join('\n'))
+            setCopied(true)
+          }}
+        />
+        {copied &&
+          <Text>
+            Copied!
+          </Text>
+        }
       </View>
     </ScrollView>
   );
